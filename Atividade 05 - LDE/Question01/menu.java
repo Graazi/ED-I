6da78f431bb4cad8a589fa406d16fba0a5473d3e
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-04-25 01:15:42 +0000

    menu/pessoas

--- Atividade 05 - LDE/Question01/menu.java
+++ Atividade 05 - LDE/Question01/menu.java
@@ -1,3 +1,16 @@
 public class menu {
+
+    public static void exibeOpcoes() {
+        System.out.println("Programa de controle de acesso.");
+        System.out.println("Opções:");
+        System.out.println("1 – Cadastrar;");
+        System.out.println("2 – Cancelar;");
+        System.out.println("3 – Exibir;");
+        System.out.println("4 – Exibir ao inverso;");
+        System.out.println("5 – Verificar;");
+        System.out.println("6 – Remover;");
+        System.out.println("0 – Encerrar o programa");
+        System.out.print("Informe a opção: ");
+    }
     
 }

6e936f5f9d517a3ef048f389afc244a0fe3fcee2
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-04-25 19:01:56 +0000

    menu

--- Atividade 05 - LDE/Question01/menu.java
+++ Atividade 05 - LDE/Question01/menu.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+
 public class menu {
 
-    public static void exibeOpcoes() {
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Pessoas p;
+        String rg, nome;
+        int op;
         
+        do {
+            exibeOpcoes();
+            op = in.nextInt(); in.nextLine();
+            switch (op) {
+                case 1: System.out.print("Informe o RG da pessoa: ");
+                        rg = in.nextLine();
+                        p = new Pessoas();
+                        System.out.print("Informe o nome da pessoa: ");
+                        nome = in.nextLine();
+                        break;
+                case 2: // Cancelar();
+                        break;
+                case 3: // exibir();
+                        break;
+                case 4: // exibiraoinverso();
+                        break;
+                case 5: // verificar();
+                        break;
+                case 6: // remover();
+                        break;
+                case 0: System.out.println("Fim de programa");
+                        break;
+                default: System.out.println("Opção inválida!");
+            }
+        } while (op != 0);
+    }
+
+    public static void exibeOpcoes() {  
         System.out.println("Programa de controle de acesso.");
         System.out.println("Opções:");
         System.out.println("1 – Cadastrar;");

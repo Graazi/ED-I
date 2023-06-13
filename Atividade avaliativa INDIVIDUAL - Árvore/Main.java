924686061f731634075eb10bf110ab9a122f6379
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-06-13 13:21:11 +0000

    ed1

---
+++ Atividade avaliativa INDIVIDUAL - Árvore/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String [] args) {
+    BinaryTree<Integer> tree = new BinaryTree <> ();
+
+
+
+    int contagemDeFolhas = tree.countfolhas();
+    System.out.println("Quantidade de folhas: " + contagemDeFolhas);
+    }
+}

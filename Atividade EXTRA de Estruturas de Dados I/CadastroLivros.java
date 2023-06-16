4a7d45d4d059e6448549a4e0ec33961acbc4743f
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-06-16 01:00:22 +0000

    ed1

--- Atividade EXTRA de Estruturas de Dados I/CadastroLivros.java
+++ Atividade EXTRA de Estruturas de Dados I/CadastroLivros.java
@@ -8,26 +8,27 @@ public class CadastroLivros {
     }
 
     public void cadastrarLivro() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Cadastro de Livro:");
-        System.out.print("ISBN: ");
-        String isbn = scanner.nextLine();
-        if (livros.search(new Livro(isbn, "", "", 0.0, 0)) == null) {
-            System.out.print("Título: ");
-            String titulo = scanner.nextLine();
-            System.out.print("Autor: ");
-            String autor = scanner.nextLine();
-            System.out.print("Preço: ");
-            double preco = scanner.nextDouble();
-            System.out.print("Quantidade em Estoque: ");
-            int quantidadeEstoque = scanner.nextInt();
-            scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Cadastro de Livro:");
+            System.out.print("ISBN: ");
+            String isbn = scanner.nextLine();
+            if (livros.search(new Livro(isbn, "", "", 0.0, 0)) == null) {
+                System.out.print("Título: ");
+                String titulo = scanner.nextLine();
+                System.out.print("Autor: ");
+                String autor = scanner.nextLine();
+                System.out.print("Preço: ");
+                double preco = scanner.nextDouble();
+                System.out.print("Quantidade em Estoque: ");
+                int quantidadeEstoque = scanner.nextInt();
+                scanner.nextLine();
 
-            Livro livro = new Livro(isbn, titulo, autor, preco, quantidadeEstoque);
-            livros.insert(livro);
-            System.out.println("Livro cadastrado com sucesso.");
-        } else {
-            System.out.println("Já existe um livro com o mesmo ISBN.");
+                Livro livro = new Livro(isbn, titulo, autor, preco, quantidadeEstoque);
+                livros.insert(livro);
+                System.out.println("Livro cadastrado com sucesso.");
+            } else {
+                System.out.println("Já existe um livro com o mesmo ISBN.");
+            }
         }
     }
 
@@ -43,10 +44,11 @@ public class CadastroLivros {
     public void alterarPrecoLivro(String isbn) {
         Livro livro = buscarLivro(isbn);
         if (livro != null) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Novo preço: ");
-            double novoPreco = scanner.nextDouble();
-            livro.setPreco(novoPreco);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Novo preço: ");
+                double novoPreco = scanner.nextDouble();
+                livro.setPreco(novoPreco);
+            }
             System.out.println("Preço do livro alterado com sucesso.");
         } else {
             System.out.println("Livro não encontrado.");
@@ -56,10 +58,11 @@ public class CadastroLivros {
     public void atualizarEstoqueLivro(String isbn) {
         Livro livro = buscarLivro(isbn);
         if (livro != null) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Quantidade para adicionar ao estoque: ");
-            int quantidade = scanner.nextInt();
-            livro.atualizarEstoque(quantidade);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Quantidade para adicionar ao estoque: ");
+                int quantidade = scanner.nextInt();
+                livro.atualizarEstoque(quantidade);
+            }
             System.out.println("Estoque do livro atualizado com sucesso.");
         } else {
             System.out.println("Livro não encontrado.");
@@ -69,10 +72,11 @@ public class CadastroLivros {
     public void venderLivro(String isbn) {
         Livro livro = buscarLivro(isbn);
         if (livro != null) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Quantidade a ser vendida: ");
-            int quantidade = scanner.nextInt();
-            livro.venderLivro(quantidade);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Quantidade a ser vendida: ");
+                int quantidade = scanner.nextInt();
+                livro.venderLivro(quantidade);
+            }
             System.out.println("Venda realizada com sucesso.");
         } else {
             System.out.println("Livro não encontrado.");

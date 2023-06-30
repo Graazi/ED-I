354e8801a14af4c6cd4803078009a88a93f339b1
Graziela de Oliveira <grazielaoliveiragg@gmail.com>
2023-06-30 18:45:34 +0000

    ed1

--- trabalhoLDEC/ListaPrincipal.java
+++ trabalhoLDEC/ListaPrincipal.java
@@ -1,5 +1,44 @@
 package trabalhoLDEC;
 
 public class ListaPrincipal {
+    NodePrincipal primeiro;
+
+    public ListaPrincipal() {
+        this.primeiro = null;
+    }
+
+
+    public void inserirLetra(char letra) {
+        NodePrincipal newNode = new NodePrincipal(letra);
+
+        if (primeiro == null) {
+            primeiro = newNode;
+            primeiro.next = primeiro;
+        } else {
+            NodePrincipal atual = primeiro;
+            while (atual.next != primeiro) {
+                atual = atual.next;
+            }
+            atual.next = newNode;
+            newNode.next = primeiro;
+        }
+    }
     
+    public boolean isEmpty() {
+        return primeiro == null;
+    }
+
+    public void exibirTermo() {
+        if (isEmpty()) {
+            System.out.println("Lista principal vazia.");
+            return;
+        }
+
+        NodePrincipal atual = primeiro;
+        do {
+            System.out.print(atual.letra + " ");
+            atual = atual.next;
+        } while (atual != primeiro);
+        System.out.println();
+    }
 }
--- trabalhoLDEC/Main.java
+++ trabalhoLDEC/Main.java
@@ -1,5 +1,51 @@
 package trabalhoLDEC;
 
+import java.util.Scanner;
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ListaPrincipal listaPrincipal = new ListaPrincipal();
+        
+
+        int op = 0;
+        do {
+            System.out.println("\n===== Menu =====");
+            System.out.println("1. Cadastrar termo;");
+            System.out.println("2. Remover termo;");
+            System.out.println("3. Exibir definição de um termo;");
+            System.out.println("4. Editar termo;");
+            System.out.println("5. Exibir os termos em ordem alfabética;");
+            System.out.println("6. Exibir termos por letra;");
+            System.out.println("0. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            op = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (op) {
+                case 1:
+                    System.out.print("Informe o nome do termo: ");
+                    String nome = scanner.nextLine();
+                    System.out.print("Informe a definição do termo: ");
+                    String definicao = scanner.nextLine();
+
+                    Termo termo = new Termo(nome, definicao);                   
+
+                    char primeiraLetra = Character.toLowerCase(nome.charAt(0));
+
+                    
+                    if (listaPrincipal.isEmpty() || !letraExists(listaPrincipal.primeiro, primeiraLetra)) {
+                        listaPrincipal.inserirLetra(primeiraLetra);
+                    }
+
+                    ListaSecundaria listaSecundaria = getListaSecundaria(listaPrincipal.primeiro, primeiraLetra);
+
+                    
+                    listaSecundaria.inserirTermo(termo);
+
+                    System.out.println("Termo cadastrado com sucesso.");
+                    break;
+        }
+    }
 }
